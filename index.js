578c72697a5dfd3719c0a04f51f9850993156d17
Subharup Chakraborty <subharup2018@gmail.com>
2023-02-21 15:08:40 +0530

    exercise 3.15

--- index.js
+++ index.js
@@ -6,97 +6,95 @@ const app = express()
 const Person = require("./models/person")
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static("build"))
+app.use(cors())
 app.use(morgan("dev"))
 
-const exampleWare = (req,res,next) => {
-    console.log("An example middleware was used in here")
-    next()
-}
-
-app.use(exampleWare)
-
-let persons = 
-[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    {
-        "id" : 5,
-        "name" : "Robert Schnell",
-        "number" : "444-4456-3562"
-    },
-    {
-        "id" : 6,
-        "name" : "Emraan Hashmi",
-        "number" : "442-5256-9295"
-    }
-]
-
 app.get("/api/persons" , (req,res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 app.get("/api/info" , (req , res) => {
-    let date = new Date()
-    Person.find({}).then(people => {
-        res.end(`Phonebook has info for ${people.length} people\n\n${date}`)
-    })
+  let date = new Date()
+  Person.find({}).then(people => {
+    res.end(`Phonebook has info for ${people.length} people\n\n${date}`)
+  })
 })
 
-app.get("/api/persons/:uid" , (req,res) => {
-    Person.findById(req.params.uid).then(person => {
-        res.json(person)
-    })
-})
-
-app.delete("/api/persons/:haturi" , (req , res) => {
-    const kolabaeng = Number(req.params.haturi)
-    console.log(kolabaeng)
-    persons = persons.filter(person => person.id !== kolabaeng)
-    res.end(`Person with id number ${kolabaeng} was removed from dictionary`)
+app.get("/api/persons/:uid" , (req,res,next) => {
+  Person.findById(req.params.uid)
+  .then(person => {
+    if(person){
+      res.json(person)
+    }
+    else{
+      res.status(404).end()
+    }
+  })
+  .catch(err => {
+    next(err)
+  })
 })
 
 app.post("/api/persons" , (req , res) => {
-
-    const body = req.body
-
-    if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+  
+  const body = req.body
+  
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ error: 'content missing' })
   }
-
+  
   const person = new Person({
     name : body.name,
     number : body.number,
   })
-
+  
   person.save().then(savedperson => {
-    response.json(savedperson)
+    res.json(savedperson)
   })
 })
+
+app.put("/api/persons/:id" , (req, res , next) => {
+  const body = req.body
+  
+  const note = {
+    name : body.name,
+    number : body.number,
+  }
+  
+  Person.findByIdAndUpdate(req.params.id , note , {new : true})
+  .then(updatedPerson => {
+    res.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
+
+app.delete("/api/persons/:haturi" , (req , res , next) => {
+  Person.findByIdAndRemove(req.params.haturi)
+  .then(result => {
+    res.status(204).end("Delete successful")
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+const errorHandler = (error , req, res , next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError'){
+    return res.status(400).send({ error : "malformatted id"})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 let PORT = process.env.PORT
 
 app.listen(PORT || 3001 , () => {
-    console.log(`Listening now at localhost:${PORT || 3001}`)
+  console.log(`Listening now at localhost:${PORT || 3001}`)
 })
